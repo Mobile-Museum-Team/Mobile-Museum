22c16b438ca6c7b81abf160f4cd2b9e59a3adfa9
Anton Lazarev <senjermen@yandex-team.ru>
2021-12-18 12:10:52 +0300

    hero for entire page

--- mobile_museum/lib/presentation/art_view_page.dart
+++ mobile_museum/lib/presentation/art_view_page.dart
@@ -19,72 +19,72 @@ class _ArtViewPageState extends State<ArtViewPage> {
   Widget build(BuildContext context) {
     final HttpService httpService = HttpService();
     //httpService.htmlParser(art);
-    return Scaffold(
-        body: CustomScrollView(slivers: <Widget>[
-      SliverAppBar(
-        backgroundColor: Colors.transparent,
-        centerTitle: false,
-        pinned: false,
-        snap: false,
-        floating: false,
-        stretch: true,
-        expandedHeight: 450.0,
-        leadingWidth: 0,
-        flexibleSpace: Stack(children: [
-          Positioned(
-            child: InkWell(
-              onTap: () {
-                Navigator.of(context).push(
-                  MaterialPageRoute(
-                    builder: (context) =>
-                        FullsreenImageView(link: widget.art.primaryImage),
-                  ),
-                );
-              },
-              child: FlexibleSpaceBar(
-                stretchModes: <StretchMode>[
-                  StretchMode.zoomBackground,
-                  StretchMode.fadeTitle,
-                ],
-                titlePadding:
-                    EdgeInsetsDirectional.only(start: 30, end: 10, bottom: 40),
-                centerTitle: false,
-                title: Container(
-                  decoration: BoxDecoration(
-                    boxShadow: [
-                      BoxShadow(
-                        color: Colors.black.withOpacity(0.7),
-                        spreadRadius: 70,
-                        blurRadius: 120,
-                        offset: Offset(10, 50), // changes position of shadow
-                      ),
-                    ],
-                  ),
-                  child: Text.rich(
-                    TextSpan(
-                      text: widget.art.title,
-                      children: [
-                        TextSpan(
-                          text: '\n' + widget.art.author,
-                          style: const TextStyle(
-                            fontWeight: FontWeight.w300,
-                            color: Colors.white,
-                            fontSize: 10,
-                          ),
+    return Hero(
+      tag: widget.art.primaryImage,
+      child: Scaffold(
+          body: CustomScrollView(slivers: <Widget>[
+        SliverAppBar(
+          backgroundColor: Colors.transparent,
+          centerTitle: false,
+          pinned: false,
+          snap: false,
+          floating: false,
+          stretch: true,
+          expandedHeight: 450.0,
+          leadingWidth: 0,
+          flexibleSpace: Stack(children: [
+            Positioned(
+              child: InkWell(
+                onTap: () {
+                  Navigator.of(context).push(
+                    MaterialPageRoute(
+                      builder: (context) =>
+                          FullsreenImageView(link: widget.art.primaryImage),
+                    ),
+                  );
+                },
+                child: FlexibleSpaceBar(
+                  stretchModes: <StretchMode>[
+                    StretchMode.zoomBackground,
+                    StretchMode.fadeTitle,
+                  ],
+                  titlePadding: EdgeInsetsDirectional.only(
+                      start: 30, end: 10, bottom: 40),
+                  centerTitle: false,
+                  title: Container(
+                    decoration: BoxDecoration(
+                      boxShadow: [
+                        BoxShadow(
+                          color: Colors.black.withOpacity(0.7),
+                          spreadRadius: 70,
+                          blurRadius: 120,
+                          offset: Offset(10, 50), // changes position of shadow
                         ),
                       ],
-                      style: TextStyle(
-                        fontWeight: FontWeight.bold,
-                        color: Colors.white,
-                        fontSize: 15,
-                      ),
                     ),
-                    textAlign: TextAlign.left,
+                    child: Text.rich(
+                      TextSpan(
+                        text: widget.art.title,
+                        children: [
+                          TextSpan(
+                            text: '\n' + widget.art.author,
+                            style: const TextStyle(
+                              fontWeight: FontWeight.w300,
+                              color: Colors.white,
+                              fontSize: 10,
+                            ),
+                          ),
+                        ],
+                        style: TextStyle(
+                          fontWeight: FontWeight.bold,
+                          color: Colors.white,
+                          fontSize: 15,
+                        ),
+                      ),
+                      textAlign: TextAlign.left,
+                    ),
                   ),
-                ),
-                background: Hero(
-                  tag: widget.art.primaryImage,
-                  child: DecoratedBox(
+                  background: DecoratedBox(
                       position: DecorationPosition.foreground,
                       decoration: BoxDecoration(
                           image: DecorationImage(
@@ -93,203 +93,203 @@ class _ArtViewPageState extends State<ArtViewPage> {
                               fit: BoxFit.cover))),
                 ),
               ),
+              top: 0,
+              left: 0,
+              right: 0,
+              bottom: 0,
             ),
-            top: 0,
-            left: 0,
-            right: 0,
-            bottom: 0,
-          ),
-          Positioned(
-            child: Container(
-              height: 30,
-              decoration: BoxDecoration(
-                color: Colors.white,
-                borderRadius: BorderRadius.vertical(
-                  top: Radius.circular(50),
+            Positioned(
+              child: Container(
+                height: 30,
+                decoration: BoxDecoration(
+                  color: Colors.white,
+                  borderRadius: BorderRadius.vertical(
+                    top: Radius.circular(50),
+                  ),
                 ),
               ),
+              bottom: -1,
+              left: 0,
+              right: 0,
             ),
-            bottom: -1,
-            left: 0,
-            right: 0,
-          ),
-        ]),
-      ),
-      SliverList(
-          delegate: SliverChildListDelegate([
-        DecoratedBox(
-          decoration: const BoxDecoration(
-            color: Colors.white,
-          ),
-          //height: 700.0,
-          //alignment: Alignment.topLeft,
-          child: Padding(
-            padding: const EdgeInsets.only(left: 30, right: 20),
-            child: Column(
-              crossAxisAlignment: CrossAxisAlignment.start,
-              children: [
-                Row(children: [
-                  Text.rich(TextSpan(
-                    text: 'Overview',
-                    style: TextStyle(
-                      fontWeight: FontWeight.bold,
-                      color: Colors.black87,
-                      fontSize: 25,
-                    ),
-                  )),
-                  Expanded(
-                    child: Padding(
-                      padding: const EdgeInsets.only(right: 30),
-                      child: Align(
-                        alignment: Alignment.centerRight,
-                        child: IconToggleButton(
-                          isSelected: isSelected,
-                          onPressed: () {
-                            setState(
-                              () {
-                                isSelected = !isSelected;
-                              },
-                            );
-                          },
+          ]),
+        ),
+        SliverList(
+            delegate: SliverChildListDelegate([
+          DecoratedBox(
+            decoration: const BoxDecoration(
+              color: Colors.white,
+            ),
+            //height: 700.0,
+            //alignment: Alignment.topLeft,
+            child: Padding(
+              padding: const EdgeInsets.only(left: 30, right: 20),
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  Row(children: [
+                    Text.rich(TextSpan(
+                      text: 'Overview',
+                      style: TextStyle(
+                        fontWeight: FontWeight.bold,
+                        color: Colors.black87,
+                        fontSize: 25,
+                      ),
+                    )),
+                    Expanded(
+                      child: Padding(
+                        padding: const EdgeInsets.only(right: 30),
+                        child: Align(
+                          alignment: Alignment.centerRight,
+                          child: IconToggleButton(
+                            isSelected: isSelected,
+                            onPressed: () {
+                              setState(
+                                () {
+                                  isSelected = !isSelected;
+                                },
+                              );
+                            },
+                          ),
                         ),
                       ),
+                    )
+                  ]),
+
+                  // ------
+                  SizedBox(height: 30),
+                  Text.rich(TextSpan(
+                    text: 'Years',
+                    style: TextStyle(
+                      fontWeight: FontWeight.normal,
+                      color: Colors.black45,
+                      fontSize: 14,
                     ),
-                  )
-                ]),
+                  )),
+                  SizedBox(height: 3),
+                  Text.rich(TextSpan(
+                    text: widget.art.date,
+                    style: TextStyle(
+                      fontWeight: FontWeight.w600,
+                      color: Colors.black87,
+                      fontSize: 16,
+                    ),
+                  )),
 
-                // ------
-                SizedBox(height: 30),
-                Text.rich(TextSpan(
-                  text: 'Years',
-                  style: TextStyle(
-                    fontWeight: FontWeight.normal,
-                    color: Colors.black45,
-                    fontSize: 14,
-                  ),
-                )),
-                SizedBox(height: 3),
-                Text.rich(TextSpan(
-                  text: widget.art.date,
-                  style: TextStyle(
-                    fontWeight: FontWeight.w600,
-                    color: Colors.black87,
-                    fontSize: 16,
-                  ),
-                )),
+                  // ------
+                  SizedBox(height: 30),
+                  Text.rich(TextSpan(
+                    text: 'Artist Bio',
+                    style: TextStyle(
+                      fontWeight: FontWeight.normal,
+                      color: Colors.black45,
+                      fontSize: 14,
+                    ),
+                  )),
+                  SizedBox(height: 3),
+                  Text.rich(TextSpan(
+                    text: widget.art.artistBio,
+                    style: TextStyle(
+                      fontWeight: FontWeight.w600,
+                      color: Colors.black87,
+                      fontSize: 16,
+                    ),
+                  )),
+                  // ------
+                  SizedBox(height: 30),
+                  Text.rich(TextSpan(
+                    text: 'Medium',
+                    style: TextStyle(
+                      fontWeight: FontWeight.normal,
+                      color: Colors.black45,
+                      fontSize: 14,
+                    ),
+                  )),
+                  SizedBox(height: 3),
+                  Text.rich(TextSpan(
+                    text: widget.art.medium,
+                    style: TextStyle(
+                      fontWeight: FontWeight.w600,
+                      color: Colors.black87,
+                      fontSize: 16,
+                    ),
+                  )),
+                  // ------
+                  SizedBox(height: 30),
+                  Text.rich(TextSpan(
+                    text: 'Dimensions',
+                    style: TextStyle(
+                      fontWeight: FontWeight.normal,
+                      color: Colors.black45,
+                      fontSize: 14,
+                    ),
+                  )),
+                  SizedBox(height: 3),
+                  Text.rich(TextSpan(
+                    text: widget.art.dimensions,
+                    style: TextStyle(
+                      fontWeight: FontWeight.w600,
+                      color: Colors.black87,
+                      fontSize: 16,
+                    ),
+                  )),
+                  //-------
+                  SizedBox(height: 30),
+                  Text.rich(TextSpan(
+                    text: 'Location',
+                    style: TextStyle(
+                      fontWeight: FontWeight.normal,
+                      color: Colors.black45,
+                      fontSize: 14,
+                    ),
+                  )),
+                  SizedBox(height: 3),
+                  Text.rich(TextSpan(
+                    text: 'Metropolitan Museum of Art, NY',
+                    style: TextStyle(
+                      fontWeight: FontWeight.w600,
+                      color: Colors.black87,
+                      fontSize: 16,
+                    ),
+                  )),
+                  //---------
+                  SizedBox(height: 30),
+                  Text.rich(TextSpan(
+                    text: 'Credit',
+                    style: TextStyle(
+                      fontWeight: FontWeight.normal,
+                      color: Colors.black45,
+                      fontSize: 14,
+                    ),
+                  )),
+                  SizedBox(height: 3),
+                  Text.rich(TextSpan(
+                    text: widget.art.creditLine,
+                    style: TextStyle(
+                      fontWeight: FontWeight.w600,
+                      color: Colors.black87,
+                      fontSize: 16,
+                    ),
+                  )),
 
-                // ------
-                SizedBox(height: 30),
-                Text.rich(TextSpan(
-                  text: 'Artist Bio',
-                  style: TextStyle(
-                    fontWeight: FontWeight.normal,
-                    color: Colors.black45,
-                    fontSize: 14,
-                  ),
-                )),
-                SizedBox(height: 3),
-                Text.rich(TextSpan(
-                  text: widget.art.artistBio,
-                  style: TextStyle(
-                    fontWeight: FontWeight.w600,
-                    color: Colors.black87,
-                    fontSize: 16,
-                  ),
-                )),
-                // ------
-                SizedBox(height: 30),
-                Text.rich(TextSpan(
-                  text: 'Medium',
-                  style: TextStyle(
-                    fontWeight: FontWeight.normal,
-                    color: Colors.black45,
-                    fontSize: 14,
-                  ),
-                )),
-                SizedBox(height: 3),
-                Text.rich(TextSpan(
-                  text: widget.art.medium,
-                  style: TextStyle(
-                    fontWeight: FontWeight.w600,
-                    color: Colors.black87,
-                    fontSize: 16,
-                  ),
-                )),
-                // ------
-                SizedBox(height: 30),
-                Text.rich(TextSpan(
-                  text: 'Dimensions',
-                  style: TextStyle(
-                    fontWeight: FontWeight.normal,
-                    color: Colors.black45,
-                    fontSize: 14,
-                  ),
-                )),
-                SizedBox(height: 3),
-                Text.rich(TextSpan(
-                  text: widget.art.dimensions,
-                  style: TextStyle(
-                    fontWeight: FontWeight.w600,
-                    color: Colors.black87,
-                    fontSize: 16,
-                  ),
-                )),
-                //-------
-                SizedBox(height: 30),
-                Text.rich(TextSpan(
-                  text: 'Location',
-                  style: TextStyle(
-                    fontWeight: FontWeight.normal,
-                    color: Colors.black45,
-                    fontSize: 14,
-                  ),
-                )),
-                SizedBox(height: 3),
-                Text.rich(TextSpan(
-                  text: 'Metropolitan Museum of Art, NY',
-                  style: TextStyle(
-                    fontWeight: FontWeight.w600,
-                    color: Colors.black87,
-                    fontSize: 16,
-                  ),
-                )),
-                //---------
-                SizedBox(height: 30),
-                Text.rich(TextSpan(
-                  text: 'Credit',
-                  style: TextStyle(
-                    fontWeight: FontWeight.normal,
-                    color: Colors.black45,
-                    fontSize: 14,
-                  ),
-                )),
-                SizedBox(height: 3),
-                Text.rich(TextSpan(
-                  text: widget.art.creditLine,
-                  style: TextStyle(
-                    fontWeight: FontWeight.w600,
-                    color: Colors.black87,
-                    fontSize: 16,
-                  ),
-                )),
-
-                SizedBox(height: 60),
-                //------
-                // SizedBox(height: 30),
-                // Text.rich(TextSpan(
-                //   text: 'Similar',
-                //   style: TextStyle(
-                //     fontWeight: FontWeight.bold,
-                //     color: Colors.black87,
-                //     fontSize: 25,
-                //   ),
-                // )),
-              ],
+                  SizedBox(height: 60),
+                  //------
+                  // SizedBox(height: 30),
+                  // Text.rich(TextSpan(
+                  //   text: 'Similar',
+                  //   style: TextStyle(
+                  //     fontWeight: FontWeight.bold,
+                  //     color: Colors.black87,
+                  //     fontSize: 25,
+                  //   ),
+                  // )),
+                ],
+              ),
             ),
           ),
-        ),
-      ]))
-    ]));
+        ]))
+      ])),
+    );
   }
 }
 
